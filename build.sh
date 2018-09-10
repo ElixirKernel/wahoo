7d183bf95fc221aef3a719fec350d55a2e00b81e
ZION959 <minhdtran43@gmail.com>
2018-09-09 21:49:37 -0700

    wahoo: add simple build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#############################################################
+## This build script will export proper paths to toolchain ##
+## and correct ARCH for device. Modify these values to fit ##
+## your environment and run it before building.            ##
+#############################################################
+
+make O=out clean && make mrproper
+make O=out ARCH=arm64 flash_defconfig
+
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=~/Android/Toolchains/clang-7.0/bin/clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=~/Android/Toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- \
+                      CROSS_COMPILE=~/Android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
