a1997027bc156b21a4df1eac34e862d2a3584fe2
ZION959 <minhdtran43@gmail.com>
2018-09-22 21:52:39 -0700

    build script: update

--- buildkernel.sh
+++ buildkernel.sh
@@ -6,18 +6,21 @@
 ##             environment and run it before building.            ##
 ####################################################################
 
+KERNEL_VER=V1.6
+
 BUILD_KERNEL_DIR=$(pwd)
 BUILD_KERNEL_OUT=$BUILD_KERNEL_DIR/../wahoo_kernel_out
 BUILD_KERNEL_OUT_DIR=$BUILD_KERNEL_OUT/KERNEL_OBJ
 BOOT_DIR=arch/arm64/boot
 DTS_DIR=arch/arm64/boot/dts/qcom
 CONFIG_DIR=arch/arm64/configs
+AK2_DIR=$BUILD_KERNEL_DIR/../AnyKernel2
 
-CLANG_TOOLCHAIN=~/Android/Toolchains/clang-7.0/bin/clang
-BUILD_CROSS_COMPILE_ARCH64=~/Android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-BUILD_CROSS_COMPILE_ARM32=~/Android/Toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
-KBUILD_COMPILER_STRING=$(${CLANG_TOOLCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export CLANG_CROSS_COMPILE=~/Android/Toolchains/clang-7.0/bin/clang
+export BUILD_CROSS_COMPILE_ARCH64=~/Android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export BUILD_CROSS_COMPILE_ARM32=~/Android/Toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export KBUILD_COMPILER_STRING=$(${CLANG_CROSS_COMPILE} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 KERNEL_DEFCONFIG=elixir_wahoo_defconfig
 KERNEL_IMG_NAME=Image.lz4-dtb
@@ -43,8 +46,7 @@ FUNCTION_GENERATE_DEFCONFIG()
         echo -e "${restore}"
 
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm64 \
-			CROSS_COMPILE=$BUILD_CROSS_COMPILE_ARCH64 \
-			$KERNEL_DEFCONFIG || exit -1
+	         CC=$CLANG_CROSS_COMPILE $KERNEL_DEFCONFIG || exit -1
 
 	cp $BUILD_KERNEL_OUT_DIR/.config $BUILD_KERNEL_DIR/$CONFIG_DIR/$KERNEL_DEFCONFIG
 
@@ -69,25 +71,20 @@ FUNCTION_BUILD_KERNEL()
 if [ "$USE_CCACHE" ]
 then
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm64 \
-            CC="ccache $CLANG_TOOLCHAIN" \
-            HOSTCC="ccache $CLANG_TOOLCHAIN" \
+            CC="ccache $CLANG_CROSS_COMPILE" \
+            HOSTCC="ccache $CLANG_CROSS_COMPILE" \
             CLANG_TRIPLE=aarch64-linux-gnu- \
             CROSS_COMPILE_ARM32=$BUILD_CROSS_COMPILE_ARM32 \
             CROSS_COMPILE=$BUILD_CROSS_COMPILE_ARCH64 || exit -1
 
 else
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm64 \
-            CC="$CLANG_TOOLCHAIN" \
-            HOSTCC="$CLANG_TOOLCHAIN" \
+            CC="$CLANG_CROSS_COMPILE" \
+            HOSTCC="$CLANG_CROSS_COMPILE" \
             CLANG_TRIPLE=aarch64-linux-gnu- \
             CROSS_COMPILE_ARM32=$BUILD_CROSS_COMPILE_ARM32 \
             CROSS_COMPILE=$BUILD_CROSS_COMPILE_ARCH64 || exit -1
 fi
-
-    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/$KERNEL_IMG_NAME $KERNEL_IMG
-    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/Image.lz4 $BUILD_KERNEL_OUT
-    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/dtbo.img $BUILD_KERNEL_OUT
-    cp $BUILD_KERNEL_OUT_DIR/$DTS_DIR/msm8998-v2.1-soc.dtb $BUILD_KERNEL_OUT
 
     echo -e "${yellow}"
     echo "Made Kernel image: $KERNEL_IMG"
@@ -99,10 +96,38 @@ fi
     echo -e "${restore}"
 }
 
+FUNCTION_MAKE_ZIP()
+{
+        echo -e "${blink_red}"
+        echo "============================"
+        echo " START : FUNCTION MAKE ZIPS "
+        echo "============================"
+        echo -e "${restore}"
+        echo -e "${cyan}"
+        echo "make boot zip = "Elixir-Wahoo-Kernel-$KERNEL_VER-`date +[%m-%d-%y-%H%M%S]` ""
+        echo -e "${restore}"
+
+    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/$KERNEL_IMG_NAME $KERNEL_IMG
+    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/Image.lz4 $AK2_DIR/kernel
+    cp $BUILD_KERNEL_OUT_DIR/$BOOT_DIR/dtbo.img $AK2_DIR
+    cp $BUILD_KERNEL_OUT_DIR/$DTS_DIR/msm8998-v2.1-soc.dtb $AK2_DIR/dtbs
+
+    cd $AK2_DIR
+    zip -r9 Elixir-wahoo-Kernel-$KERNEL_VER-`date +[%m-%d-%y-%H%M%S]`.zip * -x .git README.md *placeholder
+    mv Elixir-wahoo-Kernel-*.zip $BUILD_KERNEL_OUT
+
+	echo -e "${yellow}"
+	echo "=========================="
+	echo "  END: FUNCTION MAKE ZIPS "
+	echo "=========================="
+	echo -e "${restore}"
+}
+
 (
     START_TIME=`date +%s`
     FUNCTION_GENERATE_DEFCONFIG
     FUNCTION_BUILD_KERNEL
+    FUNCTION_MAKE_ZIP
 
     END_TIME=`date +%s`
 
