f82798c837222c004970a7ac72ed055d26b917ee
kdrag0n <dragon@khronodragon.com>
2019-01-03 18:25:00 -0800

    cpu_input_boost: refactor last input boost event variable name
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    
    # Conflicts:
    #	drivers/gpu/drm/msm/sde/sde_crtc.c
    #	kernel/fork.c

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -18,7 +18,7 @@
 #define ST_BG "background"
 #define ST_ROOT "/"
 
-unsigned long last_input_time;
+unsigned long last_input_jiffies;
 
 static __read_mostly unsigned int input_boost_freq_lp = CONFIG_INPUT_BOOST_FREQ_LP;
 static __read_mostly unsigned int input_boost_freq_hp = CONFIG_INPUT_BOOST_FREQ_PERF;
@@ -448,7 +448,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 
 	queue_kthread_work(&b->worker, &b->input_boost);
 
-	last_input_time = jiffies;
+	last_input_jiffies = jiffies;
 }
 
 static int cpu_input_boost_input_connect(struct input_handler *handler,
